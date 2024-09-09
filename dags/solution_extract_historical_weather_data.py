2c86d0a51e7ee6c323010e665da836e76635d1a4
Sam <22232647@qq.com>
2024-09-09 13:49:12 +0000

    deploy solution to dag folder

---
+++ dags/solution_extract_historical_weather_data.py
@@ -0,0 +1,143 @@
+"""DAG that retrieves weather information and saves it to duckdb."""
+
+# --------------- #
+# PACKAGE IMPORTS #
+# --------------- #
+
+from airflow.decorators import dag, task
+from airflow import Dataset
+from pendulum import datetime
+import pandas as pd
+
+# import tools from the Astro SDK
+from astro import sql as aql
+
+# -------------------- #
+# Local module imports #
+# -------------------- #
+
+from include.global_variables import airflow_conf_variables as gv
+from include.global_variables import constants as c
+from include.meterology_utils import (
+    get_lat_long_for_cityname,
+    get_historical_weather_from_city_coordinates,
+)
+
+# ----------------- #
+# Astro SDK Queries #
+# ----------------- #
+
+
+@aql.dataframe(pool="duckdb")
+def turn_json_into_table(in_json):
+    """Converts the list of JSON input into one pandas dataframe."""
+    if type(in_json) == dict:
+        df = pd.DataFrame(in_json)
+    else:
+        df = pd.concat([pd.DataFrame(d) for d in in_json], ignore_index=True)
+    return df
+
+
+# --- #
+# DAG #
+# --- #
+
+
+# ---------- #
+# Exercise 1 #
+# ---------- #
+# Schedule this DAG to run as soon as the 'start' DAG has finished running.
+# Tip: Look at how the 'extract_current_weather_data' DAG is scheduled.
+
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    # SOLUTION: schedule the DAG to run on the Dataset("start")
+    schedule=[Dataset("start")],
+    catchup=False,
+    default_args=gv.default_args,
+    description="DAG that retrieves weather information and saves it to a local JSON.",
+    tags=["part_2", "solution"],
+    # render Jinja templates as native objects (e.g. dictionary) instead of strings
+    render_template_as_native_obj=True,
+)
+def solution_extract_historical_weather_data():
+    @task
+    def get_lat_long_for_city(city):
+        """Use the 'get_lat_long_for_cityname' function from the local
+        'metereology_utils' module to retrieve the coordinates of a city."""
+
+        city_coordinates = get_lat_long_for_cityname(city)
+        return city_coordinates
+
+    @task
+    def get_historical_weather(coordinates):
+        """Use the 'get_historical_weather_from_city_coordinates' function from the local
+        'metereology_utils' module to retrieve the historical weather in a city
+        from the open-meteo API."""
+
+        historical_weather_and_coordinates = (
+            get_historical_weather_from_city_coordinates(coordinates)
+        )
+
+        return historical_weather_and_coordinates.to_dict()
+
+    # ---------- #
+    # Exercise 2 #
+    # ---------- #
+    # Modify the following two lines of code so that both the 'get_lat_long_for_city' task
+    # and the 'get_historical_weather' run on a whole list of cities. Choose 3-5 cities
+    # to retrieve historical weather data for.
+    # Tip: This task can be accomplished by using Dynamic Task Mapping.
+
+    # SOLUTION: Map both tasks using .expand. Note that the city input has to be a list of cities!
+    coordinates = get_lat_long_for_city.expand(city=["Bern", "Basel", "Zurich"])
+    historical_weather = get_historical_weather.expand(coordinates=coordinates)
+
+    @task(
+        outlets=[Dataset("duckdb://include/dwh/historical_weather_data")],
+    )
+    def turn_json_into_table(
+        duckdb_conn_id: str,
+        historical_weather_table_name: str,
+        historical_weather: list,
+    ):
+        """
+        Convert the JSON input with info about historical weather into a pandas
+        DataFrame and load it into DuckDB.
+        Args:
+            duckdb_conn_id (str): The connection ID for the DuckDB connection.
+            historical_weather_table_name (str): The name of the table to store the historical weather data.
+            historical_weather (list): The historical weather data to load into DuckDB.
+        """
+        from duckdb_provider.hooks.duckdb_hook import DuckDBHook
+
+        if type(historical_weather) == list:
+            list_of_df = []
+
+            for item in historical_weather:
+                df = pd.DataFrame(item)
+                list_of_df.append(df)
+
+            historical_weather_df = pd.concat(list_of_df, ignore_index=True)
+        else:
+            historical_weather_df = pd.DataFrame(historical_weather)
+
+        duckdb_conn = DuckDBHook(duckdb_conn_id).get_conn()
+        cursor = duckdb_conn.cursor()
+        cursor.sql(
+            f"CREATE TABLE IF NOT EXISTS {historical_weather_table_name} AS SELECT * FROM historical_weather_df"
+        )
+        cursor.sql(
+            f"INSERT INTO {historical_weather_table_name} SELECT * FROM historical_weather_df"
+        )
+        cursor.close()
+
+    turn_json_into_table(
+        duckdb_conn_id=gv.CONN_ID_DUCKDB,
+        historical_weather_table_name=c.IN_HISTORICAL_WEATHER_TABLE_NAME,
+        historical_weather=historical_weather,
+    )
+
+
+solution_extract_historical_weather_data()
---
+++ dags/solution_transform_historical_weather.py
@@ -0,0 +1,145 @@
+"""DAG that runs a transformation on data in DuckDB using the Astro SDK"""
+
+# --------------- #
+# PACKAGE IMPORTS #
+# --------------- #
+
+from airflow.decorators import dag, task
+from airflow.datasets import Dataset
+from pendulum import datetime
+import pandas as pd
+
+# import tools from the Astro SDK
+from astro import sql as aql
+from astro.sql.table import Table
+
+# -------------------- #
+# Local module imports #
+# -------------------- #
+
+from include.global_variables import airflow_conf_variables as gv
+from include.global_variables import user_input_variables as uv
+from include.global_variables import constants as c
+
+# ----------------- #
+# Astro SDK Queries #
+# ----------------- #
+
+
+# Create a reporting table that counts heat days per year for each city location
+@aql.transform(pool="duckdb")
+def create_historical_weather_reporting_table(in_table: Table, hot_day_celsius: float):
+    return """
+        SELECT time, city, temperature_2m_max AS day_max_temperature,
+        SUM(
+            CASE
+            WHEN CAST(temperature_2m_max AS FLOAT) >= {{ hot_day_celsius }} THEN 1
+            ELSE 0
+            END
+        ) OVER(PARTITION BY city, YEAR(CAST(time AS DATE))) AS heat_days_per_year
+        FROM {{ in_table }}
+    """
+
+
+# --- #
+# DAG #
+# --- #
+
+# ---------- #
+# Exercise 1 #
+# ---------- #
+# Schedule this DAG to run as soon as the 'extract_historical_weather_data' DAG has finished running.
+# Tip: You will need to use the dataset feature.
+
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    # SOLUTION: Run this DAG as soon as the historical weather data table is updated
+    schedule=[Dataset("duckdb://include/dwh/historical_weather_data")],
+    catchup=False,
+    default_args=gv.default_args,
+    description="Runs transformations on climate and current weather data in DuckDB.",
+    tags=["part_2", "solution"],
+)
+def solution_transform_historical_weather():
+
+    create_historical_weather_reporting_table(
+        in_table=Table(
+            name=c.IN_HISTORICAL_WEATHER_TABLE_NAME, conn_id=gv.CONN_ID_DUCKDB
+        ),
+        hot_day_celsius=uv.HOT_DAY,
+        output_table=Table(
+            name=c.REPORT_HISTORICAL_WEATHER_TABLE_NAME, conn_id=gv.CONN_ID_DUCKDB
+        ),
+    )
+
+    # ---------- #
+    # Exercise 3 #
+    # ---------- #
+    # One possible solution of using pandas to find the hottest day of the year
+    # for a given birth year.
+
+    @task(
+        pool="duckdb",
+    )
+    def find_hottest_day_birthyear(
+        duckdb_conn_id: str,
+        input_table_name: pd.DataFrame,
+        birthyear: int,
+        output_table_name: str,
+    ):
+
+        from duckdb_provider.hooks.duckdb_hook import DuckDBHook
+
+        duckdb_conn = DuckDBHook(duckdb_conn_id).get_conn()
+        cursor = duckdb_conn.cursor()
+        input_df = cursor.sql(
+            f"""
+            SELECT * FROM {input_table_name}
+            """
+        ).df()
+
+        df = input_df
+
+        # select the data from one year
+        try:
+            df_birthyear = df[df["time"].str.startswith(str(birthyear))]
+        except:
+            # if my birthyear is not available, use the year 2022
+            df_birthyear = df[df["time"].str.startswith(str("2022"))]
+
+        # group the data by city, use an apply function to find the row with the highest temperature
+        output_df = (
+            df_birthyear.groupby("city")
+            .apply(lambda x: x.loc[x["temperature_2m_max"].idxmax()])
+            .reset_index(drop=True)
+        )
+
+        # rename columns
+        output_df.columns = [
+            "Date hottest day",
+            "°C hottest day",
+            "City",
+            "lat",
+            "long",
+        ]
+
+        # select columns shown in output table
+        output_df = output_df[["Date hottest day", "City", "°C hottest day"]]
+
+        # saving the output_df to a new table
+        cursor.sql(
+            f"CREATE OR REPLACE TABLE {output_table_name} AS SELECT * FROM output_df"
+        )
+        cursor.sql(f"INSERT INTO {output_table_name} SELECT * FROM output_df")
+        cursor.close()
+
+    find_hottest_day_birthyear(
+        duckdb_conn_id=gv.CONN_ID_DUCKDB,
+        input_table_name=c.IN_HISTORICAL_WEATHER_TABLE_NAME,
+        birthyear=uv.BIRTH_YEAR,
+        output_table_name=c.REPORT_HOT_DAYS_TABLE_NAME,
+    )
+
+
+solution_transform_historical_weather()
