49179884379a02971757501928393d01bebbd558
Sam <22232647@qq.com>
2024-09-09 11:11:17 +0000

    Added my new DAG and Modified variables

---
+++ dags/snowflake_query_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.utils.dates import days_ago
+
+# Default args for the DAG
+default_args = {
+    'owner': 'airflow',
+}
+
+# Define the DAG
+dag = DAG(
+    'snowflake_query',
+    default_args=default_args,
+    description='Run a simple query in Snowflake',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    tags=['example'],
+)
+
+# Snowflake query
+query = """
+SELECT CURRENT_VERSION();
+"""
+
+# SnowflakeOperator task
+run_query = SnowflakeOperator(
+    task_id='run_query',
+    sql=query,
+    snowflake_conn_id='snowflake_conn_test',
+    dag=dag,
+)
+
+run_query
--- include/global_variables/user_input_variables.py
+++ include/global_variables/user_input_variables.py
@@ -2,7 +2,7 @@
 # Enter your own info! #
 # -------------------- #
 
-MY_NAME = "Friend"
-MY_CITY = "Bern"
+MY_NAME = "Sen Wang"
+MY_CITY = "Abu Dhabi"
 HOT_DAY = 30 # modify what you consider a hot day in degree Celsius
 BIRTH_YEAR = 1990
