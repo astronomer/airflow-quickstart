7356ca56ba7b8aa5849243d3e13845dccd68a3db
merobi-hub <merobi@gmail.com>
2024-10-10 09:25:17 -0400

    Sql gen fun in include.
    Signed-off-by: merobi-hub <merobi@gmail.com>

---
+++ etl/include/custom_functions/load_functions.py
@@ -0,0 +1,7 @@
+def create_sql_query(df, table_name):
+    sql_text = []
+    for index, row in df.iterrows():       
+        sql_text.append(
+                f"INSERT INTO {table_name} ("+ str(", ".join(df.columns))+ ") VALUES "+ str(tuple(row.values))
+            )        
+    return sql_text
